599b34b755f24d7a9112f43d85ce5dece9ad5eff
myfireheart <48440547+myfireheart@users.noreply.github.com>
2019-03-11 20:47:51 +0800

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -1,8 +1,8 @@
 #!/usr/bin/evn python
 #-*- coding: utf-8 -*-
 '''
-Created on 2019年1月17日 Python 2.7
-@author: flatbean 
+Created on 2019年3月11日 Python 2.7
+@author: myfireheart 
 实验要求：
 请先将本文件helloword.py上传到你新建的仓库
 上传到Github后，请修改上面的日期为当天日期，修改作者信息为你自己的版权信息，保存提交。
@@ -12,5 +12,5 @@ Created on 2019年1月17日 Python 2.7
 if __name__ == '__main__':
     print('Hello, I am first python script on Github')
     print('你好，我是在Github上的第一个python程序!\n')
-    print('你好，我的学号后四位是0101。')  
+    print('你好，我的学号后四位是0303。')  
     
