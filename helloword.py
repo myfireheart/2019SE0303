f061db0d2a0a8a7707417181586aa8960b60761f
myfireheart <48440547+myfireheart@users.noreply.github.com>
2019-03-11 20:43:03 +0800

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,16 @@
+#!/usr/bin/evn python
+#-*- coding: utf-8 -*-
+'''
+Created on 2019年1月17日 Python 2.7
+@author: flatbean 
+实验要求：
+请先将本文件helloword.py上传到你新建的仓库
+上传到Github后，请修改上面的日期为当天日期，修改作者信息为你自己的版权信息，保存提交。
+然后，请修改下面程序中的输出语句，将语句输出的学号后四位改为你的学号后四位。
+'''
+
+if __name__ == '__main__':
+    print('Hello, I am first python script on Github')
+    print('你好，我是在Github上的第一个python程序!\n')
+    print('你好，我的学号后四位是0101。')  
+    
